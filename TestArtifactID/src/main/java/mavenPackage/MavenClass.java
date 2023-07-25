2f69b09302f13f9de0f159c551114ae738075144
Lenovo <Lenovo@DESKTOP-BO3KAMP>
2023-07-25 13:09:58 +0530

    modified again

--- TestArtifactID/src/main/java/mavenPackage/MavenClass.java
+++ TestArtifactID/src/main/java/mavenPackage/MavenClass.java
@@ -5,6 +5,8 @@ public class MavenClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Maven");
 		System.out.println("Hello Maven1");
+		System.out.println("Hello Maven2");
+		
 
 	}
 
